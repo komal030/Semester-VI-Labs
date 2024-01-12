04dd3b53d3c2d5fbf1f385d6f07053a1c2db7075
komal030 <96829750+komal030@users.noreply.github.com>
2024-01-12 20:02:41 +0530

    q3

--- Parallel Programming/LAB 2/q3.c
+++ Parallel Programming/LAB 2/q3.c
@@ -1,46 +1,44 @@
 #include <stdio.h>
 #include <mpi.h>
 
-int main(int argc, char *argv[]) {
-    int size, rank;
-    MPI_Init(&argc, &argv);
-    
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Status status;
+int main(int argc, char * argv[]) {
+  int size, rank;
+  MPI_Init( & argc, & argv);
 
-    int arrsize = (size - 1) * sizeof(int);  // Adjust buffer size calculation
+  MPI_Comm_size(MPI_COMM_WORLD, & size);
+  MPI_Comm_rank(MPI_COMM_WORLD, & rank);
+  MPI_Status status;
 
-    if (rank == 0) {
-        int arr[arrsize];  // Adjust array size for root process
-        printf("Enter array elements\n");
-        
-        for (int i = 0; i < size - 1; i++)  // Adjust loop condition
-            scanf("%d", &arr[i]);
+  int buff[100];
+  int bufsize = 100;
+  if (rank == 0) {
+    int arr[size - 1];
+    printf("Enter array elements\n");
 
-        MPI_Buffer_attach(arr, arrsize);
+    for (int i = 0; i < size - 1; i++)
+      scanf("%d", & arr[i]);
 
-        for (int i = 1; i < size; i++) {
-            int ele = arr[i - 1];  // Adjust array index
-            MPI_Bsend(&ele, 1, MPI_INT, i, 1, MPI_COMM_WORLD);
-        }
+    MPI_Buffer_attach(buff, bufsize);
 
-        MPI_Buffer_detach(arr, &arrsize);
-    } 
-    else {
-        int a;
-        MPI_Recv(&a, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
-
-        if (rank % 2 == 0) {
-            a = a * a;
-            printf("Process %d: Square :%d\n", rank, a);
-        } else {
-            a = a * a * a;
-            printf("Process %d: Cube :%d\n", rank, a);
-        }
+    for (int i = 0; i < size - 1; i++) {
+      int ele = arr[i];
+      MPI_Bsend( & ele, 1, MPI_INT, i + 1, 1, MPI_COMM_WORLD);
     }
 
-    MPI_Finalize();
-    return 0;
-}
+    MPI_Buffer_detach(buff, & bufsize);
+  } else {
+    int a;
+    MPI_Recv( & a, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, & status);
 
+    if (rank % 2 == 0) {
+      a = a * a;
+      printf("Process %d: Square :%d\n", rank, a);
+    } else {
+      a = a * a * a;
+      printf("Process %d: Cube :%d\n", rank, a);
+    }
+  }
+
+  MPI_Finalize();
+  return 0;
+}
