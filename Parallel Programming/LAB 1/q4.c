1f757ddd4b4b3da28785895d4318afe2c8448c7a
komal030 <96829750+komal030@users.noreply.github.com>
2024-01-13 10:44:21 +0530

    Update q4.c

--- Parallel Programming/LAB 1/q4.c
+++ Parallel Programming/LAB 1/q4.c
@@ -1,6 +1,11 @@
+// mpirun -n 100 ./q4
 #include <mpi.h>
 #include <stdio.h>
+#include <string.h>
+
 void toggleString(char * str, int i) {
+  if (i > strlen(str))
+    return;
   if (str[i] >= 'A' && str[i] <= 'Z')
     str[i] = str[i] + 32;
   else if (str[i] >= 'a' && str[i] <= 'z')
@@ -9,14 +14,24 @@ void toggleString(char * str, int i) {
 
 int main(int argc, char * argv[]) {
   int rank, size;
+  MPI_Status status;
   MPI_Init( & argc, & argv);
   MPI_Comm_rank(MPI_COMM_WORLD, & rank);
   MPI_Comm_size(MPI_COMM_WORLD, & size);
-  char word[] = "abcd";
-  char copy1[] = "abcd";
-  toggleString(word, rank);
-  printf("Proces %d: %s\n", rank, word);
-  strcpy(word, copy1);
+  if (rank == 0) {
+    printf("Enter word: ");
+    char word[100];
+    scanf("%s", word);
+    int len = strlen(word);
+    for (int i = 0; i < len; i++) {
+      MPI_Send( & word, 100, MPI_CHAR, i + 1, 1, MPI_COMM_WORLD);
+    }
+  } else {
+    char copy[100];
+    MPI_Recv( & copy, 100, MPI_CHAR, 0, 1, MPI_COMM_WORLD, & status);
+    toggleString(copy, rank - 1);
+    printf("Proces %d: %s\n", rank, copy);
+  }
   MPI_Finalize();
   return 0;
 }
