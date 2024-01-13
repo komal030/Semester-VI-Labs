d11f39f829a79883c828adb6fa18458f0e5ccffc
komal030 <96829750+komal030@users.noreply.github.com>
2024-01-13 10:21:56 +0530

    Update q2.c

--- Parallel Programming/LAB 2/q2.c
+++ Parallel Programming/LAB 2/q2.c
@@ -7,12 +7,13 @@ int main(int argc, char * argv[]) {
   MPI_Comm_rank(MPI_COMM_WORLD, & rank);
   MPI_Comm_size(MPI_COMM_WORLD, & size);
   MPI_Status status;
-  int a;
+  int a,x=0;
   if (rank == 0) {
-    int b = 5;
-    for(int i=1;i<b-1;i++)
-    MPI_Send( & b, 1, MPI_INT, i, i, MPI_COMM_WORLD);
-    
+    for(int i=1;i<size;i++)
+    {
+    x=x+10;
+    MPI_Send( &x, 1, MPI_INT, i, i, MPI_COMM_WORLD);
+    }
   } else {
     MPI_Recv( & a, 1, MPI_INT, 0, rank, MPI_COMM_WORLD, & status);
     printf("Process %d Number: %d\n", rank, a);
