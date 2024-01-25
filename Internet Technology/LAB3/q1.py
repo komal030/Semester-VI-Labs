ca872ded75b8a56f0e2a7c4376453a2ccfd1a1bf
komal030 <96829750+komal030@users.noreply.github.com>
2024-01-25 09:41:56 +0530

    q1

---
+++ Internet Technology/LAB3/q1.py
@@ -0,0 +1,20 @@
+a=(int)(input("Enter first number:"))
+b=(int)(input("Enter second number:"))
+print("Enter\n1 for Addition\n2 for Subtraction\n3 for Multiplication\n4 for Division")
+op=int(input("Enter your choice:"))
+if(op==1):
+ d=a+b
+ print("Addition :",d)
+if(op==2):
+ d=a-b
+ print("Subtraction :",d) 
+if(op==3):
+ d=a*b
+ print("Multiplication :",d)
+if(op==4):  
+ d=a/b
+ print("Division :",d)
+
+
+
+
