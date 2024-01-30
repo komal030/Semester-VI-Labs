4c8868e31229b3826273bfc70edf85bab164f279
komal030 <96829750+komal030@users.noreply.github.com>
2024-01-30 10:17:33 +0530

    q2server.py

--- Distributed Systems/LAB 4/q2server.py
+++ Distributed Systems/LAB 4/q2server.py
@@ -10,10 +10,9 @@ sock.bind((udp_host, udp_port))
 print("####### Server is listening #######")
 
 while True:
-    msg, client_address = s.recvfrom(1024)
+    msg, client_address = sock.recvfrom(1024)
     print(f"2. Server received: {msg.decode()}")
 
     reply = input("Type some text to send => ")
-    s.sendto(reply.encode(), client_address)
+    sock.sendto(reply.encode(), client_address)
     print(f"1. Server sent: {reply}")
-
