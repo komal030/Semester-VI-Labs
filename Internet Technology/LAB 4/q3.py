2c2df048ec200dfc232c4913dded7556a8e701ce
komal030 <96829750+komal030@users.noreply.github.com>
2024-02-01 10:48:05 +0530

    q3

--- Internet Technology/LAB 4/q3.py
+++ Internet Technology/LAB 4/q3.py
@@ -1,21 +1,17 @@
-class PowerCalculator:
-    def my_pow(self, x, n):
-        result = 1
-        current_power = x
-
-        while n > 0:
-            if n % 2 == 1:
-                result *= current_power
-            current_power *= current_power
-            n //= 2
-
-        return result
-
-if __name__ == "__main__":
-    calculator = PowerCalculator()
-    
-    x = float(input("Enter the base (x): "))
-    n = int(input("Enter the exponent (n): "))
-
-    result = calculator.my_pow(x, n)
-    print(f"The result of {x}^{n} is: {result}")
+class Power:
+    def __init__(self,base,exponent):
+        self.x=base
+        self.n=exponent
+        self.res=1
+        
+    def cal(self):
+        for i in range (0,self.n):
+           self.res*=self.x
+           self.n-=1          
+        print(f"{x}^^{n}:",self.res)
+        
+if __name__=="__main__":
+    x=int(input("Enter base:"))
+    n=int(input("Enter exponent:"))
+    obj=Power(x,n)
+    obj.cal()
