50fc85529b2af4aa6772064ea51e8fba6b9b2f50
komal030 <96829750+komal030@users.noreply.github.com>
2024-02-13 10:07:46 +0530

    q1

--- Distributed Systems/LAB 6 actual/q1sepmap.py
+++ Distributed Systems/LAB 6 actual/q1sepmap.py
@@ -1,17 +1,23 @@
+'''python3 sepmap.py|sort|python3 sepred.py'''
 #!/usr/bin/env python
 import sys
+
 def read_input(file):
- for line in file:
-# split the line into words
-  yield line.split()
+    for line in file:
+        # split the line into words
+        yield line.split()
+
 def main(separator='\t'):
-# input comes from STDIN (standard input)
- data = read_input(sys.stdin)
- for words in data:
-# write the results to STDOUT (standard output);
-# what we output here will be the input for the
-# Reduce step, i.e. the input for reducer.py# tab-delimited; the trivial word count is 1
-  for word in words:
-   print ('%s%s%d' % (word, separator, 1))
+    # Open the file heart.txt for reading
+    with open('HR.txt', 'r') as file:
+        # input comes from the heart.txt file
+        data = read_input(file)
+        for words in data:
+            # write the results to STDOUT (standard output);
+            # what we output here will be the input for the
+            # Reduce step, i.e. the input for reducer.py# tab-delimited; the trivial word count is 1
+            for word in words:
+                print('%s%s%d' % (word, separator, 1))
+
 if __name__ == "__main__":
- main()
+    main()
